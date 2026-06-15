1a48ef4ed43fccc402c095303fadbc4f85eb7e16
Justin Bennett <git@just-be.dev>
2026-06-15 17:42:59 -0400

    feat(github): add GitHub Repository resource (#607)

--- packages/alchemy/src/GitHub/Providers.ts
+++ packages/alchemy/src/GitHub/Providers.ts
@@ -5,6 +5,7 @@ import * as Provider from "../Provider.ts";
 import { GitHubAuth } from "./AuthProvider.ts";
 import { Comment, CommentProvider } from "./Comment.ts";
 import * as Credentials from "./Credentials.ts";
+import { Repository, RepositoryProvider } from "./Repository.ts";
 import { Secret, SecretProvider } from "./Secret.ts";
 import { Variable, VariableProvider } from "./Variable.ts";
 
@@ -17,16 +18,21 @@ export class Providers extends Provider.ProviderCollection<Providers>()(
 export type ProviderRequirements = Layer.Services<ReturnType<typeof providers>>;
 
 /**
- * GitHub providers (Secret, Variable) plus the GitHub AuthProvider that
- * `alchemy login` discovers.
+ * GitHub providers (Comment, Repository, Secret, Variable) plus the GitHub
+ * AuthProvider that `alchemy login` discovers.
  */
 export const providers = () =>
   Layer.effect(
     Providers,
-    Provider.collection([Comment, Secret, Variable]),
+    Provider.collection([Comment, Repository, Secret, Variable]),
   ).pipe(
     Layer.provide(
-      Layer.mergeAll(CommentProvider(), SecretProvider(), VariableProvider()),
+      Layer.mergeAll(
+        CommentProvider(),
+        RepositoryProvider(),
+        SecretProvider(),
+        VariableProvider(),
+      ),
     ),
     Layer.provideMerge(Credentials.fromAuthProvider()),
     Layer.provideMerge(GitHubAuth),
--- packages/alchemy/src/GitHub/index.ts
+++ packages/alchemy/src/GitHub/index.ts
@@ -2,6 +2,7 @@ export * as Auth from "./AuthProvider.ts";
 export * from "./Comment.ts";
 export { GitHubCredentials, fromEnv, fromToken } from "./Credentials.ts";
 export * from "./Providers.ts";
+export * from "./Repository.ts";
 export * from "./Secret.ts";
 export * from "./Secrets.ts";
 export * from "./Variable.ts";
